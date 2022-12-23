3156ab16d54e8c014f671089c960b1ebe1947784
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2022-12-23 18:07:11 +0530

    create notes.txt

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 //This is a tutorial file
 //Author: Ayush Mazumdar
 console.log("Hello World");
+
+var myName = "Ayush Mazumdar";
+
+myName = "Ayush";
+
+let ourName = "Manchester United";
+
+const pi = 22 / 7;
+
+var a;
+var b = 2;
+
+a = 7;
+b = a;
+c = "I am a ";
+
+console.log(a);
+a = a + 1;
+b = b + 5;
+c = c + "String!";
+
+console.log(a);
+console.log(b);
+console.log(c);
